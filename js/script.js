a0b1e83c181973b128d6b2cdc92c85da511be3a7
Saurabhmeharkar <scmeharkarpc@gmail.com>
2020-12-20 17:40:23 +0530

    zeroone commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.onscroll = function () {
+    const docScrollTop = document.documentElement.scrollTop;
+
+    if (window.innerWidth > 200) {
+        if (docScrollTop > 100) {
+            document.querySelector("header").classList.add("fixed")
+        }
+        else {
+            document.querySelector("header").classList.remove("fixed")
+        }
+    }
+
+}
+
+//Navbar Links
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a");
+
+a.forEach(function (element) {
+    element.addEventListener("click", function () {
+        for (let i = 0; i < a.length; i++) {
+            a[i].classList.remove("active")
+        }
+        this.classList.add("active")
+        document.querySelector(".navbar").classList.toggle("show");
+    })
+})
+
+
+// Ham-Burger
+
+const hamBurger = document.querySelector(".ham-burger");
+
+hamBurger.addEventListener("click", function () {
+    document.querySelector(".navbar").classList.toggle("show");
+})
